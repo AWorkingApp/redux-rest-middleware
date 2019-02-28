697e29da1c9cd41c22d693cb2441a0c1ee46ee52
chen <tianclch@gmail.com>
2019-02-27 23:01:47 -0500

    add keywords to package.json

--- dist/redux-rest-middleware.js
+++ dist/redux-rest-middleware.js
@@ -2,7 +2,7 @@
  * redux-rest-middleware
  * A middleware to help make standard CRUD request with redux.
  * 
- * @version v1.0.0
+ * @version v1.0.1
  * @author Charlie Liu<charlie@aworkingapp.com>
  * @homepage undefined
  * @repository git+https://github.com/AWorkingApp/redux-rest-middleware.git
